9f359dc0a6c59b74eea2073362773edeaeab88b4
Raph-1201 <75044563+raph1201@users.noreply.github.com>
2023-02-04 15:53:46 -0500

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -6,17 +6,27 @@ import requests
 #Get the URL of the Ressource Manager
 cURL = pycurl.Curl()
 
-#Ping the cloud
+#1. Ping the cloud
 #Syntax : $ cloud hello
 def cloud_hello(url):
     cURL.setopt(cURL.URL, url)
     cURL.perform()
- 
-#Register new node, possibility to give pod name 
+
+#2. Register new pod, must give pod name
+#Syntax : $ cloud pod register <pod name>
+def cloud_pod_register(url, command):
+    command_list = command.split()
+
+    if len(command_list) == 4:
+        cURL.setopt(cURL.URL, url + '/cloud/pods/' + command_list[3])
+        cURL.perform()
+
+
+#4. Register new node, possibility to give pod name
 #Syntax : $ cloud register <node_name> <pod_name>
 def cloud_register(url, command):
     command_list = command.split()
-    
+
     #If no pod name given
     if len(command_list) == 3:
         cURL.setopt(cURL.URL, url + '/cloud/nodes/' + command_list[2])
@@ -27,16 +37,19 @@ def cloud_register(url, command):
         cURL.setopt(cURL.URL, url + '/cloud/nodes/' + command_list[2] + '/' + command_list[3])
         cURL.perform()
 
+
 #Send files to cloud, this is where we will input bash scripts
 #Syntax : $cloud launch <file_name.sh>
 def cloud_launch(url, command):
     command_list = command.split()
+
     if len(command_list) == 3:
         file_path = command_list[2]
         if (os.path.isfile(file_path)):
             files = {'file': open(file_path, 'rb')}
             ret = requests.post(url + '/cloud/jobs/launch', files=files)
             print(ret.text)
+
 
 #Main function
 #This is where we put the different 
@@ -46,10 +59,16 @@ def main():
         command = input('$ ')
         if command == 'exit':
             exit()
+
         elif command == 'cloud hello':
             cloud_hello(rm_url)
+
+        elif command.startswith('cloud pod register'):
+            cloud_pod_register(rm_url, command)
+
         elif command.startswith('cloud register'):
             cloud_register(rm_url, command)
+
         elif command.startswith('cloud launch'):
             cloud_launch(rm_url, command)
 
